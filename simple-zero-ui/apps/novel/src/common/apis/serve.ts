0b2a3b60768445adabdfb2bb0b9ca193f19896fb
leiax00 <leiax00@outlook.com>
2022-12-23 14:40:45 +0800

    szui-novel: 更新axios的baseUrl

--- simple-zero-ui/apps/novel/src/common/apis/serve.ts
+++ simple-zero-ui/apps/novel/src/common/apis/serve.ts
@@ -1,9 +1,20 @@
 import axios from 'axios'
 
+const getBaseUrl = () => {
+  const domain = import.meta.env.VITE_APP_DOMAIN.endsWith('/')
+    ? import.meta.env.VITE_APP_DOMAIN.slice(
+        0,
+        import.meta.env.VITE_APP_DOMAIN.length - 1
+      )
+    : import.meta.env.VITE_APP_DOMAIN
+  const prefix = import.meta.env.VITE_APP_PREFIX.startsWith('/')
+    ? import.meta.env.VITE_APP_PREFIX.slice(1)
+    : import.meta.env.VITE_APP_PREFIX
+  return `${domain}/${prefix}`
+}
+
 const serve = axios.create({
-  baseURL: `${import.meta.env.VITE_APP_DOMAIN}${
-    import.meta.env.VITE_APP_PREFIX
-  }`,
+  baseURL: getBaseUrl(),
   timeout: 10 * 1000,
   headers: {},
 })
