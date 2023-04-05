1c06d32637fe9a40d9b2679e278b69712c8ebe86
leiax00 <leiax00@outlook.com>
2023-04-05 17:23:13 +0800

    首页增加route列表

--- simple-zero-ui/apps/main/src/views/Home.vue
+++ simple-zero-ui/apps/main/src/views/Home.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="main-home">
-    <el-button type="primary">Hello, Home</el-button>
+    <div class="route-list">
+      <sz-link
+        v-for="item in appStore.config.menus"
+        :key="item.id"
+        type="route"
+        :to="item.path"
+      >
+        <el-card v-ripple shadow="always">{{ item.name }}</el-card>
+      </sz-link>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useApp } from '@/store/app'
+
 defineOptions({ name: 'Home' })
+const appStore = useApp()
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.main-home {
+  .route-list {
+    @apply common-grid-1;
+    .sz-link {
+      @apply w-full;
+    }
+  }
+}
+</style>
