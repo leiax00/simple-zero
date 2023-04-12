f58cb4abc89510a13dfd47d218bec72bd77a58c2
leiax00 <leiax00@outlook.com>
2023-04-12 12:55:48 +0800

    子应用根样式定义

--- simple-zero-ui/apps/novel/src/App.vue
+++ simple-zero-ui/apps/novel/src/App.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { qiankunWindow } from 'vite-plugin-qiankun/es/helper'
+
+const appClazz = computed(() => {
+  const classList = ['app-main']
+  if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+    classList.push('root-with-no-micro')
+  }
+  return classList
+})
+</script>
 
 <template>
-  <router-view :key="$route.fullPath" />
+  <router-view :key="$route.fullPath" :class="appClazz" />
 </template>
 
 <style scoped></style>
