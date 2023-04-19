9a6ae455c7e4cbe6ecc2521a2094f9e9db36a83f
leiax00 <leiax00@outlook.com>
2023-04-19 10:14:14 +0800

    更新python启动脚本

--- deploy/bash/start_py.sh
+++ deploy/bash/start_py.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-pwd
-echo "$WORK_DIR"/.venv/bin/activate
-
-python -m venv .venv
-source "$WORK_DIR"/.venv/bin/activate
-
-python -m pip install --upgrade pip
+#python -m pip install --upgrade pip
 pip install -r requirement.txt \
             -i https://pypi.tuna.tsinghua.edu.cn/simple  \
             --trusted-host pypi.tuna.tsinghua.edu.cn
