3b5556bc640fd97de499c24f0895bcaf1ab1a7d2
leiax00 <leiax00@outlook.com>
2023-06-14 15:23:10 +0800

    sz-novel: 通过虚拟环境启动应用

--- deploy/bash/start_py.sh
+++ deploy/bash/start_py.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#python -m pip install --upgrade pip
+python -m venv sz
+source sz/bin/activate
+python -m pip install --index-url https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
 pip install -r requirement.txt \
             -i https://pypi.tuna.tsinghua.edu.cn/simple  \
             --trusted-host pypi.tuna.tsinghua.edu.cn
