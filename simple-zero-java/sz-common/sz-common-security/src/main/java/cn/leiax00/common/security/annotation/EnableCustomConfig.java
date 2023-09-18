16b82e9bf22a9cf2cccdfc3689ee2b5148848dc0
leiax00 <leiax00@outlook.com>
2023-09-18 17:55:18 +0800

    java: 增加相关基础配置-1

---
+++ simple-zero-java/sz-common/sz-common-security/src/main/java/cn/leiax00/common/security/annotation/EnableCustomConfig.java
@@ -0,0 +1,25 @@
+package cn.leiax00.common.security.annotation;
+
+import cn.leiax00.common.security.config.ApplicationConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+// 表示通过aop框架暴露该代理对象,AopContext能够访问
+@EnableAspectJAutoProxy(exposeProxy = true)
+// 指定要扫描的Mapper类的包的路径
+@MapperScan("cn.leiax00.**.mapper")
+// 开启线程异步执行
+@EnableAsync
+// 自动加载类
+@Import({ApplicationConfig.class})
+public @interface EnableCustomConfig {
+
+}
