b4daf9c8befd7d29cb8e5c593fcf4863d50c57e3
leiax00 <leiax00@outlook.com>
2023-10-25 18:13:52 +0800

    增加日志记录模块

---
+++ simple-zero-java/sz-common/sz-common-log/src/main/java/cn/leiax00/common/log/service/LogService.java
@@ -0,0 +1,17 @@
+package cn.leiax00.common.log.service;
+
+import cn.leiax00.system.api.domain.SysOperLog;
+import org.springframework.stereotype.Service;
+
+/**
+ * 异步调用日志服务
+ *
+ * @author ruoyi
+ */
+@Service
+public class LogService {
+
+    public void saveSysLog(SysOperLog operLog) {
+        // todo 待实现
+    }
+}
