525b27d95de21e09eb8847da1b9947d2f4dfe841
leiax00 <leiax00@outlook.com>
2023-10-31 14:52:24 +0800

    响应对象的构造方法优化

--- simple-zero-java/sz-common/sz-common-core/src/main/java/cn/leiax00/common/core/web/domain/R.java
+++ simple-zero-java/sz-common/sz-common-core/src/main/java/cn/leiax00/common/core/web/domain/R.java
@@ -34,26 +34,13 @@ public class R<T> implements Serializable {
         return restResult(data, SUCCESS, null);
     }
 
-    public static <T> R<T> ok(T data, String msg) {
-        return restResult(data, SUCCESS, msg);
-    }
-
     public static <T> R<T> fail(String msg) {
         return restResult(null, FAIL, msg);
     }
 
-    public static <T> R<T> fail(T data) {
-        return restResult(data, FAIL, null);
+    public static <T> R<T> fail(int code, String msg) {
+        return restResult(null, code, msg);
     }
-
-    public static <T> R<T> fail(T data, String msg) {
-        return restResult(data, FAIL, msg);
-    }
-
-    public static <T> R<T> fail(T data, int code, String msg) {
-        return restResult(data, code, msg);
-    }
-
 
     private static <T> R<T> restResult(T data, int code, String msg) {
         R<T> apiResult = new R<>();
