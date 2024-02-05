a8205c57d8bdcaf14b5a45063d4370c7521054e1
leiax00 <leiax00@outlook.com>
2024-02-05 11:56:59 +0800

    调整搜索引擎的快捷键切换代码

--- simple-zero-ui/apps/main/src/components/search/EnginSearch.vue
+++ simple-zero-ui/apps/main/src/components/search/EnginSearch.vue
@@ -1,19 +1,24 @@
 <template>
-  <el-tabs v-model="data.searchEngin.active" type="border-card" @tab-change="focusInput(enginIndex)">
-    <el-tab-pane v-for="item in data.searchEngin.enginList" :key="item.key" :name="item.key" :label="item.label">
-      <el-input
-        ref="search"
-        v-model="data.searchKey"
-        :placeholder="item.placeholder"
-        clearable
-        @keyup.enter="toSearch(item.uri, data.searchKey)"
-      />
-    </el-tab-pane>
-  </el-tabs>
+  <div>
+    <div class="relative float-end top-3 right-5 z-10 text-gray-600 text-xs">Ctrl+K / Ctrl+Alt+K</div>
+    <el-tabs v-model="data.searchEngin.active" type="border-card" @tab-change="focusInput(enginIndex)">
+      <el-tab-pane v-for="item in data.searchEngin.enginList" :key="item.key" :name="item.key" :label="item.label">
+        <el-input
+          ref="search"
+          v-model="data.searchKey"
+          :placeholder="item.placeholder"
+          clearable
+          @keyup.enter="toSearch(item.uri, data.searchKey)"
+        />
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { computed, onMounted } from 'vue'
+import { useMagicKeys } from '@vueuse/core'
+import type { InputInstance } from 'element-plus'
 import type { Engin } from '@/beans/engin'
 import { useApp } from '@/store/app'
 import { searchEngin, toSearch } from '@/components/search/engin'
@@ -50,7 +55,7 @@ const changeSearchEngin = function (reverse = false) {
 
 const focusInput = function (index: number | string) {
   nextTick(() => {
-    search.value[index].focus()
+    search.value && (search.value[index] as InputInstance).focus()
   })
 }
 
@@ -61,10 +66,17 @@ onMounted(() => {
   focusInput(enginIndex.value)
 })
 
-document.addEventListener('keydown', (e) => {
-  if ((e.ctrlKey || e.altKey) && e.key === 'k') {
-    e.preventDefault()
-    changeSearchEngin(e.altKey)
+const { current } = useMagicKeys({
+  passive: false,
+  onEventFired(e) {
+    if (e.ctrlKey && e.key === 'k') {
+      e.preventDefault()
+    }
+  },
+})
+watch(current, (v) => {
+  if (v.has('control') && v.has('k')) {
+    changeSearchEngin(v.has('alt'))
   }
 })
 </script>
