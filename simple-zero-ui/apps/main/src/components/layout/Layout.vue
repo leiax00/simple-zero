739b08edffab883c91489018c65fd82f52dee8f4
leiax00 <leiax00@outlook.com>
2024-03-28 17:55:23 +0800

    修改主题加载机制

--- simple-zero-ui/apps/main/src/components/layout/Layout.vue
+++ simple-zero-ui/apps/main/src/components/layout/Layout.vue
@@ -8,14 +8,14 @@
 </template>
 
 <script setup lang="ts">
+import layoutTheme from './layoutTheme'
 import { useApp } from '@/store/app'
 
 defineOptions({ name: 'Layout' })
 const { config } = useApp()
 
 const layoutName = config.common?.theme || 'MinimalistLayout'
-const layoutPath = `./${layoutName.charAt(0).toLowerCase() + layoutName.slice(1)}/${layoutName}.vue`
-const layout = shallowRef(defineAsyncComponent(() => import(/* @vite-ignore */ layoutPath)))
+const layout = shallowRef(layoutTheme[layoutName])
 </script>
 
 <style scoped></style>
